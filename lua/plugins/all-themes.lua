5a3a205e20054bc84144b63f9dfabe998787fc5c
itsPoipoi <poipoigit@gmail.com>
2026-01-02 21:41:02 +0100

    ...

--- lua/plugins/all-themes.lua
+++ lua/plugins/all-themes.lua
@@ -90,12 +90,6 @@ return {
     opts = {},
   },
   {
-    "somerocketeer/nagai-poolside.nvim",
-    lazy = true,
-    priority = 1000,
-    opts = {},
-  },
-  {
     "bjarneo/ethereal.nvim",
     lazy = true,
     priority = 1000,
