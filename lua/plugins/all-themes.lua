82b6314d6b2564de933d72ee78d1d3668b59bf28
itsPoipoi <poipoigit@gmail.com>
2026-01-08 22:03:10 +0100

    ...

--- lua/plugins/all-themes.lua
+++ lua/plugins/all-themes.lua
@@ -28,11 +28,28 @@ return {
   },
   {
     "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "Mofiqul/dracula.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "Mofiqul/dracula.nvim",
+    name = "dracula",
     lazy = true,
     priority = 1000,
   },
   {
     "tahayvr/vhs80.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "loctvl842/monokai-pro.nvim",
     lazy = true,
     priority = 1000,
   },
@@ -47,6 +64,11 @@ return {
     priority = 1000,
   },
   {
+    "kepano/flexoki-neovim",
+    name = "flexoki",
+    priority = 1000,
+  },
+  {
     "folke/tokyonight.nvim",
     name = "tokyonight",
     lazy = true,
@@ -56,14 +78,6 @@ return {
     "ficcdaf/ashen.nvim",
     lazy = true,
     priority = 1000,
-  },
-  {
-    "Mofiqul/dracula.nvim",
-    lazy = true,
-    priority = 1000,
-    opts = {
-      transparent_bg = true,
-    },
   },
   {
     "navarasu/onedark.nvim",
