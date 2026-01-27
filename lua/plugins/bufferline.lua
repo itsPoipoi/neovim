4d7c7e59ffff0dab0c7f4c4d23fff974966508fe
itsPoipoi <poipoigit@gmail.com>
2026-01-27 12:37:39 +0100

    Add bufferline picker and opts

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,21 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      themable = true,
+      numbers = "ordinal",
+      move_wraps_at_ends = false,
+    },
+  },
+  keys = {
+    { "<A-1>", "<Cmd>BufferLineGoToBuffer 1<CR>", desc = "Go to Buffer 1" },
+    { "<A-2>", "<Cmd>BufferLineGoToBuffer 2<CR>", desc = "Go to Buffer 2" },
+    { "<A-3>", "<Cmd>BufferLineGoToBuffer 3<CR>", desc = "Go to Buffer 3" },
+    { "<A-4>", "<Cmd>BufferLineGoToBuffer 4<CR>", desc = "Go to Buffer 4" },
+    { "<A-5>", "<Cmd>BufferLineGoToBuffer 5<CR>", desc = "Go to Buffer 5" },
+    { "<A-6>", "<Cmd>BufferLineGoToBuffer 6<CR>", desc = "Go to Buffer 6" },
+    { "<A-7>", "<Cmd>BufferLineGoToBuffer 7<CR>", desc = "Go to Buffer 7" },
+    { "<A-8>", "<Cmd>BufferLineGoToBuffer 8<CR>", desc = "Go to Buffer 8" },
+    { "<A-9>", "<Cmd>BufferLineGoToBuffer 9<CR>", desc = "Go to Buffer 9" },
+  },
+}
