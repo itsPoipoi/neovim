44d5368bbc5bd1faf848e9081b5c47ecc740cbff
itsPoipoi <poipoigit@gmail.com>
2026-02-01 00:01:20 +0100

    Add missing projects keymap in dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -20,6 +20,12 @@ return {
           },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           {
+            icon = " ",
+            key = "p",
+            desc = "Projects",
+            action = ":lua Snacks.picker.projects()",
+          },
+          {
             icon = " ",
             key = "g",
             desc = "Find Text",
