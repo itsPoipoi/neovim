c58050a2908d65518b81bb38f57fa6b719aad98a
itsPoipoi <poipoigit@gmail.com>
2026-02-13 14:54:32 +0100

    Add nvim-spider

---
+++ lua/plugins/spider.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "chrisgrieser/nvim-spider",
+    lazy = true,
+    opts = {
+      skipInsignificantPunctuation = true,
+      subwordMovement = true,
+      consistentOperatorPending = false, -- see the README for details
+      customPatterns = {}, -- see the README for details
+    },
+
+    keys = {
+      { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+      { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+      { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+      { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+    },
+  },
+}
