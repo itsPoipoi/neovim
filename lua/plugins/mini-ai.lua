b4b397db42aad5edc15d0dbf86643e1b5fb7ef1d
itsPoipoi <poipoigit@gmail.com>
2026-02-23 19:01:58 +0100

    Include leading whitespaces instead of trailing

--- lua/plugins/mini-ai.lua
+++ lua/plugins/mini-ai.lua
@@ -7,17 +7,17 @@ return {
           return {
             {
               -- pattern, [^_]pattern_*
-              "%f[%d_%-%.%,]%d+[_%-%.%,%s]*",
-              "%f[%a_%-%.%,]%l+[_%-%.%,%s]*",
-              "%f[%u_%-%.%,]%u%f[%A][_%-%.%,%s]*",
-              "%f[%u_%-%.%,]%u%l+[_%-%.%,%s]*",
-              "%f[%u_%-%.%,]%u%u+[_%-%.%,%s]*",
+              "%f[%d_%-%.%,%s]%d+[_%-%.%,%s]*",
+              "%f[%a_%-%.%,%s]%l+[_%-%.%,%s]*",
+              "%f[%u_%-%.%,%s]%u%f[%A][_%-%.%,%s]*",
+              "%f[%u_%-%.%,%s]%u%l+[_%-%.%,%s]*",
+              "%f[%u_%-%.%,%s]%u%u+[_%-%.%,%s]*",
               -- __pattern
-              "%f[_%-%.%,][_%-%.%,]+%d+",
-              "%f[_%-%.%,][_%-%.%,]+%l+",
-              "%f[_%-%.%,][_%-%.%,]+%u%f[%A]",
-              "%f[_%-%.%,][_%-%.%,]+%u%l+",
-              "%f[_%-%.%,][_%-%.%,]+%u%u+",
+              "%f[_%-%.%,%s][_%-%.%,%s]+%d+",
+              "%f[_%-%.%,%s][_%-%.%,%s]+%l+",
+              "%f[_%-%.%,%s][_%-%.%,%s]+%u%f[%A]",
+              "%f[_%-%.%,%s][_%-%.%,%s]+%u%l+",
+              "%f[_%-%.%,%s][_%-%.%,%s]+%u%u+",
             },
           }
         end
@@ -25,7 +25,7 @@ return {
           local reg = MiniAi.find_textobject("a", id, opts)
           if reg then
             local line = vim.fn.getline(reg.from.line)
-            local _, s = line:find("^[_%-%.%,]*.", reg.from.col)
+            local _, s = line:find("^[_%-%.%,%s]*.", reg.from.col)
             local e = line:sub(1, reg.to.col):find(".[_%-%.%,%s]*$")
             return vim.tbl_deep_extend("force", reg, { from = { col = s }, to = { col = e } })
           end
