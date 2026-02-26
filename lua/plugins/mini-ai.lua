b9d5e1db9d8964e5df7517cd2cc4fec48891cfcb
itsPoipoi <poipoigit@gmail.com>
2026-02-26 22:47:28 +0100

    Match contiguous punctuation

--- lua/plugins/mini-ai.lua
+++ lua/plugins/mini-ai.lua
@@ -4,13 +4,18 @@ return {
     custom_textobjects = {
       e = function(ai_type, id, opts) -- original word textobject
         if ai_type == "a" then
-          return { "%f[%w_][%w_]+%s?" }
+          return {
+            {
+              "%f[%w_][%w_]+%s?",
+              "%f[%p][^%w%s_]+%s?",
+            },
+          }
         end
         if ai_type == "i" then
           local reg = MiniAi.find_textobject("a", id, opts)
           if reg then
             local line = vim.fn.getline(reg.from.line)
-            local _, s = line:find("^[_%-%.%,%s]*.", reg.from.col)
+            local _, s = line:find("^%s*.", reg.from.col)
             local e = line:sub(1, reg.to.col):find(".%s?$")
             return vim.tbl_deep_extend("force", reg, { from = { col = s }, to = { col = e } })
           end
@@ -27,12 +32,14 @@ return {
               "%f[%u_%-%.%,%s]%u%f[%A][_%-%.%,%s]?",
               "%f[%u_%-%.%,%s]%u%l+[_%-%.%,%s]?",
               "%f[%u_%-%.%,%s]%u%u+[_%-%.%,%s]?",
+              "%f[%p_%-%.%,%s][^%w%s]+[_%-%.%,%s]?",
               -- __pattern
               "%f[_%-%.%,%s][_%-%.%,%s]+%d+",
               "%f[_%-%.%,%s][_%-%.%,%s]+%l+",
               "%f[_%-%.%,%s][_%-%.%,%s]+%u%f[%A]",
               "%f[_%-%.%,%s][_%-%.%,%s]+%u%l+",
               "%f[_%-%.%,%s][_%-%.%,%s]+%u%u+",
+              "%f[_%-%.%,%s][_%-%.%,%s]+[^%w%s]+",
             },
           }
         end
@@ -41,8 +48,8 @@ return {
           if reg then
             local line = vim.fn.getline(reg.from.line)
             local _, s = line:find("^[_%-%.%,%s]*.", reg.from.col)
-            local w = line:sub(1, reg.to.col):find(".[_%-%.%,%s]?$")
-            return vim.tbl_deep_extend("force", reg, { from = { col = s }, to = { col = w } })
+            local e = line:sub(1, reg.to.col):find(".[_%-%.%,%s]?$")
+            return vim.tbl_deep_extend("force", reg, { from = { col = s }, to = { col = e } })
           end
         end
       end,
